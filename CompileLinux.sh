df273578cf91177e7a67d66aa3a419e5d31b2a8a
Technetium <Technetium1@users.noreply.github.com>
2019-10-22 15:38:55 -0400

    Version 1.1 Supporting Linux

---
+++ CompileLinux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [[ "$(python3 -V)" =~ "Python 3" ]]; then
+	echo "Python 3 is installed"
+else
+	read -p "Python 3 is not installed! Press [Enter] to exit!"
+fi
+cd `pwd`
+read -p "After installing requirements.txt with \"pip install -r /path/to/requirements.txt\" press [Enter] to continue building the executable!"
+pyinstaller -F -i web.ico --clean VideoSpider.py
+mv `pwd`/dist/VideoSpider `pwd`
+rm -rf __pycache__ build dist VideoSpider.spec
